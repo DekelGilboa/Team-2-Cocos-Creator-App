5bbbd2404eb275b75e3d7cfd66ed33b9a91bd18c
DavidGer2000 <164003542+DavidGer2000@users.noreply.github.com>
2024-03-20 12:30:08 +0200

    document Update game.ts

--- assets/Scripts/game.ts
+++ assets/Scripts/game.ts
@@ -12,6 +12,7 @@ import { getArrayOf5RandomLocations } from "./main";
 
 @ccclass("game")
 export class game extends Component {
+
   readonly speed: number = 150;
 
   maxScore: number = 0;
@@ -22,60 +23,83 @@ export class game extends Component {
   missed: boolean = false;
   isGameOver: boolean = false;
 
+  // get random starting position for the game items 
   public startLocations = [...getArrayOf5RandomLocations()];
 
   static moveUp: any;
 
   onLoad() {
     this.node.parent.parent.getChildByName("blue_bg").active = true;
+
+    // Set the initial positions of all child nodes (the items like apples, banana...) based on the startLocations array
     this.node.children.forEach((item, index) => {
       item.setPosition(this.startLocations[index]);
     });
 
-    this.node.parent.parent
-      .getChildByName("retry")
-      .on(Node.EventType.MOUSE_DOWN, () => {
-        this.startGame();
-      });
+   //get listener started
+    this.addListeners()
   }
 
+  //everytime the game updates, move the game items 
   update(deltaTime: number) {
+    
     if (!this.isGameOver) {
-      // console.log("game is running");
+
       if (!this.isEventAdded && this.node.children.length > 4) {
         this.addListeners();
         this.isEventAdded = true;
       }
-      const birdWidthArr = [0, 0, 0, 0, 0];
 
+    // Creating an array for the items width
+      const itemsWidthArr = [0, 0, 0, 0, 0];
+
+      // Creating an array of random values ​​for the location of the items
       const randomNumbers = [];
       for (let i = 0; i < 5; i++) {
         const randomNumber = Math.floor(Math.random() * (750 - 500 + 1)) + 500;
         randomNumbers.push(randomNumber);
       }
+
       const randomY = [...randomNumbers];
+
       this.node.children.forEach((item, index) => {
-        birdWidthArr[index] = item.getComponent(UITransform).width;
+        itemsWidthArr[index] = item.getComponent(UITransform).width;
         this.startLocations.push(this.node.children[index].position);
+
+         //update location 
         this.startLocations[index].y -= this.gameSpeed * deltaTime;
-        item.setPosition(this.startLocations[index]);
-        if (this.startLocations[index].y <= 0 - birdWidthArr[index]) {
+
+
+        // if item went out of bounds return it to the top
+        if (this.startLocations[index].y <= 0 - itemsWidthArr[index]) {
           this.startLocations[index].y = randomY[index];
+          // If it's not the bomb Take life off
           if (index != 4) this.lifeCounter--;
         }
+
+        // set the items in current location 
         item.setPosition(this.startLocations[index]);
       });
+
+
+      // set the life hearts in thier location
       this.node.parent
         .getChildByName("lives")
         .children.forEach((item, index) => {
+
+          // if lives are lost, hide hearts
           if (index > this.lifeCounter) {
             item.active = false;
           }
+
+          // if there is no more life left call gameover()
           if (this.lifeCounter < 0 && !this.isGameOver) {
             this.isGameOver = true;
             this.gameOver();
           }
         });
+
+        // if retry Butoon Clicked call startGame() and hide the retry Butoon
       if (
         this.node.parent.parent
           .getChildByName("retry")
@@ -90,6 +114,8 @@ export class game extends Component {
   }
 
   addListeners() {
+
+  
     game.moveUp = (index: number) => {
       const randomNumbers = [];
       for (let i = 0; i < 5; i++) {
@@ -103,6 +129,9 @@ export class game extends Component {
       this.gameSpeed += 10;
     };
 
+/* If the item were pressed call moveUp() increaseSpeed() The score will go up,
+But if it's the bomb the lifeCounter will go down
+*/
     this.node.children.forEach((item, index) => {
       if (index == 4) {
         item.on(Node.EventType.MOUSE_DOWN, () => {
@@ -118,38 +147,60 @@ export class game extends Component {
         });
       }
     });
+
+    //  When the retry button is pressed it will call to startGame()
+    this.node.parent.parent
+      .getChildByName("retry")
+      .on(Node.EventType.MOUSE_DOWN, () => {
+        this.startGame();
+      });
   }
 
+  // when the game over, show result and pause the game 
   gameOver() {
     this.showResult();
     director.pause();
     this.node.children.forEach((item) => {
+      // revealed the retry button and hide the game item
       item.active = false;
       this.node.parent.parent.getChildByName("retry").active = true;
     });
   }
 
+
+  //what to do when the game is starting.
   startGame() {
     this.isGameOver = false;
+
+    //  revealed  the game item
     this.node.parent.getChildByName("lives").children.forEach((item) => {
       item.active = true;
     });
     this.lifeCounter = 3;
     this.gameSpeed = this.speed;
     this.currentScore = 0;
+
+    //run  the game 
     director.resume();
+
+    // set teh game item in their location
     this.node.children.forEach((item, index) => {
       item.active = true;
       game.moveUp(index);
     });
+
+  
     this.node.parent.parent
       .getChildByName("result")
       .getComponent(Label).string = "Result: ";
+
+      // hide the retry button and result 
     this.node.parent.parent.getChildByName("result").active = false;
     this.node.parent.parent.getChildByName("retry").active = false;
-    console.log("game started");
   }
 
+
+ //show the score results when the game ends.
   showResult() {
     console.log("show result");
     this.maxScore = Math.max(this.maxScore, this.currentScore);
