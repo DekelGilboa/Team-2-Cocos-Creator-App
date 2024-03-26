4723114255ae310e80bf39a1b3989614f04e1d46
mendiYakobov <mendyy2000@gmail.com>
2024-03-26 11:37:11 +0200

    back btn 0.11

--- assets/Scripts/retryBtn.ts
+++
@@ -1,9 +0,0 @@
-import { _decorator, Component, director, Node, find } from "cc";
-const { ccclass, property } = _decorator;
-
-@ccclass("retryBtn")
-export class retryBtn extends Component {
-  start() {}
-
-  update(deltaTime: number) {}
-}
